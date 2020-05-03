5a7150a3325b868921c807c231fe3f0ec2b14481
Viktor Sjöblom <Viktorsjoblom93@gmail.com>
2020-05-03 12:51:09 +0200

    build

---
+++ build/precache-manifest.115146dee4b3bde401a850ebe330e4ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6929e45cda912d9e9fe435df6a4f34fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f139192c8fd592cb3297",
+    "url": "/static/css/main.f4ff033d.chunk.css"
+  },
+  {
+    "revision": "4c1f7c133c0f53851b94",
+    "url": "/static/js/2.73eb4715.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.73eb4715.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f139192c8fd592cb3297",
+    "url": "/static/js/main.72a15228.chunk.js"
+  },
+  {
+    "revision": "52ee0122b222786f3a2a",
+    "url": "/static/js/runtime-main.8453fa37.js"
+  }
+]);
